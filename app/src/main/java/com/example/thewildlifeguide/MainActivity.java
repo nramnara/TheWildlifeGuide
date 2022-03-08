15696ee0a59c6e0e0320866c5ebd1b88cdb0f59e
nick <nramnara@my.yorku.ca>
2022-03-08 17:18:20 -0500

    First commit

---
+++ app/src/main/java/com/example/thewildlifeguide/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.thewildlifeguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /* Hides the bar at the top of phone */
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        /* Hides ActionBar at the top of the app */
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+    }
+}
