9e0df23e6bb7a231ba8fa66d554be28a74166e0f
JadePablo <98762929+JadePablo@users.noreply.github.com>
2022-03-30 22:23:32 -0400

    Update SettingsPage.java

--- app/src/main/java/com/example/thewildlifeguide/SettingsPage.java
+++ app/src/main/java/com/example/thewildlifeguide/SettingsPage.java
@@ -61,7 +61,7 @@ public class SettingsPage extends AppCompatActivity {
         //check if prevAnimalName was assigned anything, if so then the previous page must be an animal info page, send the user back to that specific animal page
         if (prevAnimalName.length() > 0){
             Intent intent = new Intent(this,InfoPage.class);
-            intent.putExtra("animalName",prevAnimalName.toLowerCase(Locale.ROOT)); //lowercase the animal name, since the file name is in all lowercase
+            intent.putExtra("animalName",prevAnimalName.toLowerCase()); //lowercase the animal name, since the file name is in all lowercase
             startActivity(intent);                                                       //ex. "Cat" -> "cat" since the name for the cats .txt file for cat is in all lowercase
         }
 
