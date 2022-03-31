5370eb7dfb450ce25bbe8375d44014c5ad3d2d3f
JadePablo <98762929+JadePablo@users.noreply.github.com>
2022-03-30 22:18:36 -0400

    Update SettingsPage.java

--- app/src/main/java/com/example/thewildlifeguide/SettingsPage.java
+++ app/src/main/java/com/example/thewildlifeguide/SettingsPage.java
@@ -43,7 +43,33 @@ public class SettingsPage extends AppCompatActivity {
 
     // take the user to the homepage (MainActivity) activity
     public void openHomePage(){
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
+        //check if catched intent is from animals page
+        //make a new throw intent that goes to the info page
+        // if not (then its from the homepage)
+        //make a new throw intent that goes to the homepage
+
+        //initialize what the previous animalName is
+        String prevAnimalName;
+        prevAnimalName = "";
+
+        //check if an intent is thrown in here (is only possible if user comes from InfoPage), if so set the prev animal name to the string assigned to the key
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            prevAnimalName = extras.getString("fromInfo");
+        }
+
+        //check if prevAnimalName was assigned anything, if so then the previous page must be an animal info page, send the user back to that specific animal page
+        if (prevAnimalName.length() > 0){
+            Intent intent = new Intent(this,InfoPage.class);
+            intent.putExtra("animalName",prevAnimalName.toLowerCase(Locale.ROOT)); //lowercase the animal name, since the file name is in all lowercase
+            startActivity(intent);                                                       //ex. "Cat" -> "cat" since the name for the cats .txt file for cat is in all lowercase
+        }
+
+        //if the above is not the case, then the user must have came from the homepage, send the user back to the home page
+        else
+        {
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
