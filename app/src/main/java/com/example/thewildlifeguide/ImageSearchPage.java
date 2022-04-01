54c979804d44cc55526279c85a3ae71ba6f1a483
JadePablo <98762929+JadePablo@users.noreply.github.com>
2022-04-01 19:15:52 -0400

    added: imageSearch

--- app/src/main/java/com/example/thewildlifeguide/ImageSearchPage.java
+++ app/src/main/java/com/example/thewildlifeguide/ImageSearchPage.java
@@ -1,16 +1,34 @@
 package com.example.thewildlifeguide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ImageSearchPage extends AppCompatActivity {
+
+    //initialize the recyclerView
+    RecyclerView imageSearch;
+
+    //initialize list containing the animal's names and their corresponding pics
+    List<String> animalNames;
+    List<Drawable> animalPics;
+
+    //initialize adapter for imageSearch
+    ImagePage_Adapter imagePageAdapter;
+
+    //initialize listener for clicks
+    private ImagePage_Adapter.RecyclerViewClickListener listener;
+
+    //----------------------------------
 
     //initialize image search buttons
     private Button textSearchButton;
@@ -22,8 +40,6 @@ public class ImageSearchPage extends AppCompatActivity {
 
     //initialize the search page recycler view and listener
     private RecyclerView recyclerView;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +57,29 @@ public class ImageSearchPage extends AppCompatActivity {
 
         //-------------------------------------
 
+        //set the array to animalsList
+        list = new AnimalList(this);
+        animalList = list.getListOfAnimals();
+
+        imageSearch = findViewById(R.id.imageSearch_recyclerView);
+
+        //populate the animal's names and pics lists
+        animalNames = new ArrayList<>();
+        animalPics = new ArrayList<>();
+
+        animalNames.addAll(animalList);
+
+        for (int i = 0; i < animalList.size();i++)
+        {
+            Animal currentAnimal = new Animal (this, animalList.get(i));
+            animalPics.add(currentAnimal.getImage());
+        }
+
+        //when page is called,set the adapter using setAdapter()
+        setAdapter();
+
+
+        //----------------------------------
 
         //set value to text search button
         textSearchButton = (Button) findViewById(R.id.textsearchButton);
@@ -69,6 +108,25 @@ public class ImageSearchPage extends AppCompatActivity {
         });
     }
 
+    private void setAdapter() {
+        setOnclickListener();
+        ImagePage_Adapter imagePageAdapter = new ImagePage_Adapter(this, animalNames, animalPics,listener);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),2,GridLayoutManager.VERTICAL,false);
+        imageSearch.setLayoutManager(gridLayoutManager);
+        imageSearch.setAdapter(imagePageAdapter);
+    }
+
+    private void setOnclickListener() {
+        listener = new ImagePage_Adapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+                Intent intent = new Intent(getApplicationContext(),InfoPage.class);
+                intent.putExtra("animalName",animalList.get(position));
+                intent.putExtra("whereFrom","imageSearch");
+                startActivity(intent);
+            }
+        };
+    }
     //take the user to the text search page
     public void openTextSearch(){
         Intent intent = new Intent(this,SearchPage.class);
